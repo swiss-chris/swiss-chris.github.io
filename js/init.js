529366e264e82421570cd111724bc482475a7879
Chris Dickinson <chris.dickinson@gmx.ch>
2019-07-20 22:09:10 +0100

    new handling for contact form on statically hosted website

--- js/init.js
+++ js/init.js
@@ -14,7 +14,6 @@
 	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
 	 }, 100);
 
-
 /*----------------------------------------------------*/
 /* Smooth Scrolling
 ------------------------------------------------------ */
@@ -31,7 +30,6 @@
 	        window.location.hash = target;
 	    });
 	});
-
 
 /*----------------------------------------------------*/
 /* Highlight the current section in the navigation bar
@@ -59,7 +57,6 @@
 
 	});
 
-
 /*----------------------------------------------------*/
 /*	Make sure that #header-background-image height is
 /* equal to the browser height.
@@ -71,7 +68,6 @@
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
    });
-
 
 /*----------------------------------------------------*/
 /*	Fade In/Out Primary Navigation
@@ -97,7 +93,6 @@
 
 	});
 
-
 /*----------------------------------------------------*/
 /*	Modal Popup
 ------------------------------------------------------*/
@@ -116,7 +111,6 @@
     		e.preventDefault();
     		$.magnificPopup.close();
     });
-
 
 /*----------------------------------------------------*/
 /*	Flexslider
@@ -138,51 +132,26 @@
 ------------------------------------------------------*/
 
    $('form#contactForm button.submit').click(function() {
-
       $('#image-loader').fadeIn();
-
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
-
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
       $.ajax({
 
 	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
+         url: 'https://app.99inbound.com/api/e/__ftz8uC',
+         data: $('#contactForm').serialize(),
+	      success: function() {
+            $('#image-loader').fadeOut();
+            $('#message-warning').hide();
+            $('#contactForm').fadeOut();
+            $('#message-success').fadeIn();
+         },
+         error: function(){
+            $('#image-loader').fadeOut();
+            $('#message-warning').html('Something went wrong. Please try again.');
+	         $('#message-warning').fadeIn();
+         }
 
       });
       return false;
    });
 
-
 });
-
-
-
-
-
-
-
-
